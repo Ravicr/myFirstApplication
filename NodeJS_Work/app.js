18f0ce3f819f3fcf03c48e2440fcc6f50625a584
Ravicr <49303009+Ravicr@users.noreply.github.com>
2021-04-27 22:09:17 +0200

    Update app.js
    Updated with DB Connection

--- NodeJS_Work/app.js
+++ NodeJS_Work/app.js
@@ -1,159 +1,164 @@
-//ThirdParty Module
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
 const express = require('express');
-const bodyParser = require('body-parser');
-//const wayscript = require('wayscript');
-const request1 = require('request-promise');
+const mysql = require('promise-mysql');
+
 const app = express();
-const path = require('path');
-const expressValidator = require('express-validator');
-const expressSession = require('express-session');
-const ls = require('local-storage');
-const store = require('store2');
 
-const morgan = require('morgan');
-const uuid = require('uuid');
-const headers = require('./webpage');
+app.enable('trust proxy');
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
+// Automatically parse request body as form data.
+app.use(express.urlencoded({extended: false}));
+// This middleware is available in Express v4.16.0 onwards
+app.use(express.json());
 
-app.set( "view engine", "ejs");
-app.use(express.static('img'));
-app.use(express.static('css'));
-app.use(express.static('js'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-var urlencoded = bodyParser.urlencoded({ extended: false });
-app.use(expressSession({secret: 'max', saveUninitialized:false, resave:false}));
-//Middleware
-app.use(morgan('dev'));
-
-/*app.use((req,res,next) => {
-    console.log("Entered MiddleWare");
-    next();
-});*/
-
-const myPersons =
-[
-    {
-        id:uuid.v4(),
-        name:"Ravi",
-        age:"31"
-    },
-    {
-       id:uuid.v4(),
-       name:"Raji",
-       age:"28"
-    },
-    {
-        id:uuid.v4(),
-        name:"Yazhini",
-        age:"03"
-    },
-]
-
-//Home Page
-app.get('/',(req,res) =>{
-    console.log("Response value is  "+req);
-    var existing = ls.get('Distrubance');
-    console.log(existing)
-    res.render('index',{data : existing});
-
+// Set Content-Type for all responses for these routes.
+app.use((req, res, next) => {
+  res.set('Content-Type', 'text/html');
+  next();
 });
 
-app.post('/',urlencoded,(req,res) =>{
-    //console.log(req.body);
 
-    if(ls.get('Distrubance') == null){
-        ls.set('Distrubance', []);
-    }
-    var existing = ls.get('Distrubance');
-    existing.push(req.body);
-    ls.set('Distrubance', existing);
-    console.log("Local Storage Value ",ls.get('Distrubance'));
-    const slackbody = {
-        mkdwn: true,
-        text : 'This is a really great to post in slack',
-        attachments: [
-            {
-                mrkdwn_in: ["text"],
-                color : "#FFBE33",
-                title: "Disturbance",
-                text: "Problem with Order Placement with country",
-                author_name: "Ravichandiran Rajendiran",
-                fields:[{
-                    title: "Problem Description",
-                    value: "RU Market PayOrder Issue, so customer unable to access payorder link. Its complete down",
-                    short: false
-                    },
-                    {
-                    title: "Problem Reported time",
-                    value: "Issue start time : 09:45 CET",
-                    short: true
-                    },
-                    {
-                        title: "Impacted Markets",
-                        value: "Russia",
-                        short: true
-                    },
-                    {
-                        title: "Business Impact",
-                        value: "customer unable to access payorder link. Its complete down",
-                        short: false
-                    },
-                    {
-                        title: "Root Cause ",
-                        value: "Currently Investigation Ongoing with Order Capture and downstream system. We will keep update on this.",
-                        short: false
-                    }
+// [START cloud_sql_mysql_mysql_create_tcp]
 
-                    ],
-                    footer:"footer",
-                    footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
-                    ts: 123456789
+// [END cloud_sql_mysql_mysql_create_tcp]
 
-            }]
+// [START cloud_sql_mysql_mysql_create_socket]
+const createUnixSocketPool = async config => {
+  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
 
-    };
-    //Post to slack
-    const res1= request1({
-    url: 'https://hooks.slack.com/services/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',
-    method: 'POST',
-    body: slackbody,
-    json: true
+  // Establish a connection to the database
+return await mysql.createPool({
+    user: 'root', // e.g. 'my-db-user'
+    password: '', // e.g. 'my-db-password'
+    database: 'isharing_communication', // e.g. 'my-database'
+    // If connecting via unix domain socket, specify the path
+    socketPath: `${dbSocketPath}/meta-altar-295911:europe-west1:isharing-communication`,
+    // Specify additional properties here.
+    ...config,
+  });
+ // console.log(connection);
+ // return connection;
+};
+// [END cloud_sql_mysql_mysql_create_socket]
+
+const createPool = async () => {
+  const config = {
+    // [START cloud_sql_mysql_mysql_limit]
+    // 'connectionLimit' is the maximum number of connections the pool is allowed
+    // to keep at once.
+    connectionLimit: 5,
+    // [END cloud_sql_mysql_mysql_limit]
+
+    // [START cloud_sql_mysql_mysql_timeout]
+    // 'connectTimeout' is the maximum number of milliseconds before a timeout
+    // occurs during the initial connection to the database.
+    connectTimeout: 10000, // 10 seconds
+    // 'acquireTimeout' is the maximum number of milliseconds to wait when
+    // checking out a connection from the pool before a timeout error occurs.
+    acquireTimeout: 10000, // 10 seconds
+    // 'waitForConnections' determines the pool's action when no connections are
+    // free. If true, the request will queued and a connection will be presented
+    // when ready. If false, the pool will call back with an error.
+    waitForConnections: true, // Default: true
+    // 'queueLimit' is the maximum number of requests for connections the pool
+    // will queue at once before returning an error. If 0, there is no limit.
+    queueLimit: 0, // Default: 0
+    // [END cloud_sql_mysql_mysql_timeout]
+
+    // [START cloud_sql_mysql_mysql_backoff]
+    // The mysql module automatically uses exponential delays between failed
+    // connection attempts.
+    // [END cloud_sql_mysql_mysql_backoff]
+  };
+
+    return await createUnixSocketPool(config);
+
+};
+
+
+const createPoolAndEnsureSchema = async () =>
+  await createPool()
+    .then(async pool => {
+      //await ensureSchema(pool);
+      return pool;
+    })
+    .catch(err => {
+      logger.error(err);
+      throw err;
     });
 
-    res.render('index',{data : ls.get('Distrubance')});
+// Set up a variable to hold our connection pool. It would be safe to
+// initialize this right away, but we defer its instantiation to ease
+// testing different configurations.
+let pool;
+
+app.use(async (req, res, next) => {
+  if (pool) {
+    return next();
+  }
+  try {
+    pool = await createPoolAndEnsureSchema();
+    next();
+  } catch (err) {
+    logger.error(err);
+    return next(err);
+  }
+});
+
+// Serve the index page, showing vote tallies.
+app.get('/', async (req, res) => {
+    pool = pool || (await createPoolAndEnsureSchema());
+   // res.status(200).send();
+
+    try {
+        // Get the 5 most recent entry.
+        const recentQuery = pool.query(
+          'SELECT count(*) FROM production_disturbances'
+        );
+
+        // Run queries concurrently, and wait for them to complete
+        // This is faster than await-ing each query object as it is created
+        const recentVotes = await recentQuery;
+
+
+      } catch (err) {
+        console.log(err);
+        res
+          .status(500)
+          .send(
+            'Unable to load page. Please check the application logs for more details.'
+          )
+          .end();
+      }
+
 });
 
 
-//Issue Page
-app.get('/openIssue',(req,res) =>{
-    res.render("openIssue");
 
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
 
-//Report Page
-app.get('/reportPage',(req,res) =>{
-     var existing = ls.get('Distrubance');
-        console.log(existing)
-        res.render('reportview',{data : existing});
-
-});
-//Get All
-app.get('/about',(req,res) =>{
-    res.status(200).json(myPersons);
+process.on('unhandledRejection', err => {
+  console.error(err);
+  throw err;
 });
 
-//Get
-app.get('/:id',async(req,res) =>{
-const getOne = await myPersons.filter(e =>e.id === req.params.id)
-res.status(200).json(getOne);
-});
-
-//Local host
-const PORT = process.env.PORT || 2500;
-
-app.listen(PORT,() => {
-console.log(`Server Started On ${PORT}`);
-});
+module.exports = server;
